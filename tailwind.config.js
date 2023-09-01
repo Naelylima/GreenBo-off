b0604d8fc591323ed4ce2125298897e8eaf3bdcc
Naely Lima Pereira <110494299+Naelylima@users.noreply.github.com>
2023-09-01 11:19:10 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ export default {
   theme: {
     extend: {
       backgroundImage:{
-        'degrade': "url('/src/assets/degrade.png')"
+        'degrade':"url('./src/assets/degrade.png')",
+        'fundo':"url('./src/assets/fundo.png')",
+        'color': "url('/src/assets/fundoColor.png)",
+        'capovila': "url('./src/assets/capo.jpg')",
+        'naely': "url('./src/assets/naely.jpg')",
+        'michael': "url('./src/assets/michael.jpg')",
+        'luiza': "url('./src/assets/luiza.jpg')",
+
       }
     },
   },
